2e698e9ddf3a0ea75be7cf4c4beaacca1e4650f4
EnniKelloniemi <enni.kelloniemi@gmail.com>
2022-05-18 20:50:48 +0300

    kursivi

--- futurama/futurama.js
+++ futurama/futurama.js
@@ -2,6 +2,9 @@ fetch("https://futuramaapi.herokuapp.com/api/quotes")
     .then(response => response.json())
     .then(
         data =>{
+            function italicstyle(){
+
+            }
             //Data haettu ->käsittele
             //luodaan ul-elementti
             let ul =document.createElement('ul');
@@ -9,11 +12,15 @@ fetch("https://futuramaapi.herokuapp.com/api/quotes")
             data.forEach(quote =>{
                 //Luo li-elementti
                 let li =document.createElement('li');
+
                 //Aseta sisältö li-elementti
-                li.innerHTML=quote.character + " " + "'" + quote.quote + "'";
+                quote.quote.style.fontStyle="italic";
+                li.innerHTML= quote.character + ": '" + quote.quote + "'" +  + "'";
+                quote.style.fontStyle="italic";
                 //Lisää li-elementti ul-elementille
                 ul.appendChild(li);
             });
+            
             //Lisää ul-elemantti domiin
             document.getElementById("print-here").appendChild(ul);
             
